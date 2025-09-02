bbb9528030e07a5cc4d7ef2d29b2045065aabb51
trilok singh rana <triloksinghrana309@gmail.com>
2025-09-02 17:15:42 +0530

    my code

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get('/file/:filename',(req,res)=>{
     res.render('show',{filename: req.params.filename, filedata: req.params.filedata})
   })
 })
+app.get('/edit/:filename',(req,res)=>{
+    res.render('edit',{filename: req.params.filename})
+})
+app.post('/edit',(req,res)=>{
+  fs.rename(`./files/${req.body.previous}` , `./files/${req.body.new}`,function(err){
+    res.redirect("/")
+  })
+})
 
 app.post('/create',(req,res)=>{
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
